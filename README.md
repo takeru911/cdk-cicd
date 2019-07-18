cf468eba78b9e84776b1bc7a7f1d6205eba8f1e9
takeru911 <t.maehara911@gmail.com>
2019-07-18 14:04:16 +0900

    update readme

--- README.md
+++ README.md
@@ -1,58 +1,24 @@
+## CDKによるcode build/pipelineを利用したCI/CDの実現するやつ
 
-# Welcome to your CDK Python project!
+https://dev.classmethod.jp/server-side/serverless/aws-cdk-cicd/
+をpython & Githubのリポジトリを利用する実装に変えた。
+基本的に上の記事を見ればわかるかと
 
-This is a blank project for Python development with CDK.
+### run
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the .env
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
-
-To manually create a virtualenv on MacOS and Linux:
+* 依存ライブラリのinstall
 
 ```
-$ python3 -m venv .env
-```
-
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
-
-```
+$ virtualenv .env
 $ source .env/bin/activate
-```
-
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```
-% .env\Scripts\activate.bat
-```
-
-Once the virtualenv is activated, you can install the required dependencies.
-
-```
 $ pip install -r requirements.txt
 ```
 
-At this point you can now synthesize the CloudFormation template for this code.
+* cloudformationのstack作成し、deploy
 
 ```
-$ cdk synth
+$ cdk deploy
 ```
+(事前にcdk bootstrapの必要があるかもしれないです)
 
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
-
-# Useful commands
-
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
-
-Enjoy!
+build対象のリポジトリは https://github.com/takeru911/cdk-lambda です。
--- my_cicd/my_cicd_stack.py
+++ my_cicd/my_cicd_stack.py
@@ -1,5 +1,17 @@
-from aws_cdk import core
+from aws_cdk import(
+     core,
+     aws_lambda,
+     aws_codebuild as codebuild,
+     aws_codepipeline as codepipeline,
+     aws_codepipeline_actions as codepipeline_actions,
+     aws_iam as iam,
+     aws_secretsmanager as secretsmanager
+)
 
+owner =  "takeru911"
+repo_name = "cdk-lambda"
+tag = "my-cicd"
+target_function_name = "sample-function"
 
 class MyCicdStack(core.Stack):
 
@@ -7,3 +19,82 @@ class MyCicdStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        for stage in ["prd", "dev"] :
+            target_function = self.create_function(stage)
+            project = self.create_project(target_function, stage)
+            source_output = codepipeline.Artifact(self.create_name(stage));
+            branch = "master" if stage == "prd" else "develop"
+            codepipeline.Pipeline(self, self.create_id("Pipeline", stage),
+                                  pipeline_name=self.create_name(stage),
+                                  stages=[
+                                      codepipeline.StageProps(
+                                          stage_name="Source",
+                                          actions=[self.create_source_action(branch, source_output)]),
+                                      codepipeline.StageProps(
+                                          stage_name="Build",
+                                          actions=[self.create_build_action(project, source_output)])
+                                  ])
+
+    def create_id(self, name, stage):
+        return "-".join([tag, name, stage])
+
+    def create_name(self, stage):
+        return "_".join([tag, stage])
+        
+    def create_function(self, stage):
+        return aws_lambda.Function(
+            self,
+            self.create_id("Target", stage),
+            function_name=target_function_name + "_" + stage,
+            code=aws_lambda.Code.asset("dmy"),
+            handler="main.handle",
+            runtime=aws_lambda.Runtime.PYTHON_3_6
+            )
+
+    def create_project(self, target_function, stage):
+        project = codebuild.PipelineProject(
+            self,
+            self.create_id("Project", stage),
+            project_name=self.create_name(stage),
+            environment_variables={
+                "FUNCTION_NAME": codebuild.BuildEnvironmentVariable(
+                    value=target_function.function_name,
+                    type=codebuild.BuildEnvironmentVariableType.PLAINTEXT),
+                "STAGE": codebuild.BuildEnvironmentVariable(
+                    value=stage,
+                    type=codebuild.BuildEnvironmentVariableType.PLAINTEXT)
+            }
+        )
+        project.add_to_role_policy(
+            iam.PolicyStatement(
+                resources=[target_function.function_arn],
+                actions=['lambda:UpdateFunctionCode',
+                         'lambda:UpdateFunctionConfiguration']
+                )
+            )
+        return project
+
+    def create_source_action(self, branch, source_output):
+        secret = secretsmanager.Secret.from_secret_attributes(self,
+                                                              branch + "_secret",
+                                                              secret_arn="arn:aws:secretsmanager:ap-northeast-1:044768335503:secret:github-api-token-wtevPt")
+        oauth_token = secret.secret_value_from_json("github-api-token")
+
+        return codepipeline_actions.GitHubSourceAction(
+            action_name="GithubRepo",
+            oauth_token=oauth_token,
+            output=source_output,
+            owner=owner,
+            repo=repo_name,
+            branch=branch
+        )
+
+    def create_build_action(self, project, source_output):
+        action_name = "CodeBuild"
+        return codepipeline_actions.CodeBuildAction(
+            action_name=action_name,
+            project=project,
+            input=source_output,
+            outputs=[codepipeline.Artifact("Test")]
+        )
+
